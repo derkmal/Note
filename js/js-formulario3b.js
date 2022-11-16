ff614044ebe32462d316835708ad669a5e6390ca
darkmal <vagnerwillians@hotmail.com>
2022-11-16 15:53:30 -0300

    16-11 15:53hs

--- js/js-formulario3b.js
+++ js/js-formulario3b.js
@@ -1,10 +1,48 @@
 document.getElementById('idButton').addEventListener('click', einfugenText);
+document.getElementById('idButtonMatch').addEventListener('click', matchTextArea);
+document.getElementById('idButtonMatchP').addEventListener('click', matchTextP);
+document.getElementById('idButtonSearch').addEventListener('click', matchSearch);
+
+//Get match of textarea - REGEX
+function matchTextArea() {
+    var valueTarea = document.getElementById('idTextarea').value;
+    const regex = /China/;
+    const isExisting = regex.test(valueTarea);
+    console.log(isExisting);
+}
+//Get match of p element - REGEX
+function matchTextP() {
+    var valuePelement = document.querySelectorAll(".classP");
+    var newValue = [];
+    const regex = /China/;
+    var isExisting;
+    for(text of valuePelement) {
+        newValue.push(text.innerText);
+        isExisting = regex.test(newValue);
+        console.log(newValue);
+    };
+    console.log(isExisting);
+}
+//Get match of search element - REGEX
+function matchSearch() {
+    valuePelement = document.querySelectorAll(".classP");
+    var newValue = [];
+    const regex = /China/;
+    var isExisting;
+    for(text of valuePelement) {
+        newValue.push(text.innerText);
+        isExisting = regex.test(newValue);
+        console.log(newValue);
+    };
+    console.log(isExisting);
+}
 
 //ADD EVENT
 function einfugenText(e) {
     e.preventDefault();
     //GET INPUT VALUE
     var valueTarea = document.getElementById('idTextarea').value;
+    
 
     if(valueTarea == "") {
         alert("Bitte schreiben Sie einen Text innerhab des Kastens.");
@@ -15,6 +53,8 @@ function einfugenText(e) {
         newP.appendChild(document.createTextNode(valueTarea));
         //SET CLASS TO newP
         newP.className = 'classP';
+        //SET ID TO newP
+        newP.setAttribute('id', 'newPId');
         //CREATE LI ELEMENT
         var newLi = document.createElement("li");
         //SET ID TO newLi
@@ -27,34 +67,45 @@ function einfugenText(e) {
         var newUl = document.getElementById('idUl');
         newUl.appendChild(newLi);
         //CREATE DISPLAY BUTTON
-        var displayButton = document.createElement('button');
-        //SET CLASS TO displayButton
-        displayButton.className = 'classDisplayButton';
-        //SET ID TO displayButton
-        displayButton.setAttribute('id', 'idDisplay');
-        //SET TYPE to displayButton
-        displayButton.setAttribute('type', 'button');
-        //ADD NAME TO displayButton
-        displayButton.appendChild(document.createTextNode('View Detail'));        
-        //ADD displayButton TO newLi
-        newLi.appendChild(displayButton);
-        //DISPLAY BOX
-        displayButton.addEventListener('click', displayBox);
-        function displayBox() {
-            if(displayButton.classList.contains('classDisplayButton')) {
-                modal.style.display = "block";
-                modalContent.style.display = "block";
-                span.style.display = "block";
-                clone.style.display = "block";
-            }
-        }
-       
+        var viewDetailButton = document.createElement('button');
+        //SET CLASS TO viewDetailButton
+        viewDetailButton.classList.add("classDetailButton");
+        //SET ID TO viewDetailButton
+        viewDetailButton.setAttribute('id', 'idDisplay');
+        //SET TYPE to viewDetailButton
+        viewDetailButton.setAttribute('type', 'button');
+        //ADD NAME TO viewDetailButton
+        viewDetailButton.appendChild(document.createTextNode('View Detail'));        
+        //ADD viewDetailButton TO newLi
+        newLi.appendChild(viewDetailButton);
+        
         //GET MODAL
         var modal = document.getElementById('myModal');
         //GET SPAN
         var span = document.getElementsByClassName('close')[0];
         //GET MODAL CONTENT
         var modalContent = document.getElementById('idModal-content');
+        
+        //CREATE CLONE OF newP
+        var node = newLi.firstChild; //oder [0]
+        var clone = node.cloneNode(true);
+        //SET newLi.firstChild(p element) CLONE ID 
+        clone.setAttribute('id', 'idClone');
+        //SET CLASS TO newLi.firstChild(p element)
+        clone.className = "classPClone";
+        //ADD newLi CLONE TO modalContent
+        modalContent.appendChild(clone);    
+        
+        //DISPLAY BOX
+        viewDetailButton.addEventListener('click', displayBox);
+        function displayBox() {
+            if(viewDetailButton.classList.contains('classDetailButton')) {
+                modal.style.display = "block";
+                modalContent.style.display = "block";
+                span.style.display = "block";
+                clone.style.display = "block";
+            }
+        }
          
         //CLOSE BOX
         span.addEventListener('click', closeBox);
@@ -65,16 +116,6 @@ function einfugenText(e) {
                 clone.style.display = "none";
             }
         }
-
-        //CREATE CLONE OF newP
-        var node = newLi.firstChild; //oder [0]
-        var clone = node.cloneNode(true);
-        //SET newLi.firstChild(p element) CLONE ID 
-        clone.setAttribute('id', 'idClone');
-        //SET CLASS TO newLi.firstChild(p element)
-        clone.className = "classPclone";
-        //ADD newLi CLONE TO modalContent
-        modalContent.appendChild(clone);    
         
         //CLEAR TEXT AREA 
         var clearInput = document.getElementById("idTextarea");
@@ -82,33 +123,30 @@ function einfugenText(e) {
     }
 }
 
-
-
 //TOTAL OF WORDS AND VOWELS
-document.getElementById('idButton').addEventListener('click', countWort);
+// document.getElementById('idButton').addEventListener('click', countWort);
 
-function countWort() {
-    var text = document.getElementById('idTextarea').value;
-    var textString = text.toString();
-    var textArray = Array.from(textString);
-    var noText = 0;
-    var totalWort = 0;
-    var countVowel = 0;
-    for(var i = 0; i < textArray.length; i++) {
-        if(textArray[i] == " ") { //Mit array verwenden wir " ", mit text verwenden wir "";
-            noText++;
-        }
-        if((textArray[i] == "a") || (textArray[i] == "A") || (textArray[i] == "e") || 
-        (textArray[i] == "E") || (textArray[i] == "i") || (textArray[i] == "I") || 
-        (textArray[i] == "o") || (textArray[i] == "O") || (textArray[i] == "u") || 
-        (textArray[i] == "U")) {
-            countVowel++;
-        }  
-    }
-    console.log("die Gesamtzahl von Wörter ist:" + (totalWort = textArray.length - noText));
-    console.log("die Gesamtzahl von Vokale ist:" + (countVowel));
-}
-
+// function countWort() {
+//     var text = document.getElementById('idTextarea').value;
+//     var textString = text.toString();
+//     var textArray = Array.from(textString);
+//     var noText = 0;
+//     var totalWort = 0;
+//     var countVowel = 0;
+//     for(var i = 0; i < textArray.length; i++) {
+//         if(textArray[i] == " ") { //Mit array verwenden wir " ", mit text verwenden wir "";
+//             noText++;
+//         }
+//         if((textArray[i] == "a") || (textArray[i] == "A") || (textArray[i] == "e") || 
+//         (textArray[i] == "E") || (textArray[i] == "i") || (textArray[i] == "I") || 
+//         (textArray[i] == "o") || (textArray[i] == "O") || (textArray[i] == "u") || 
+//         (textArray[i] == "U")) {
+//             countVowel++;
+//         }  
+//     }
+//     console.log("die Gesamtzahl von Wörter ist:" + (totalWort = textArray.length - noText));
+//     console.log("die Gesamtzahl von Vokale ist:" + (countVowel));
+// }
 
 
 
